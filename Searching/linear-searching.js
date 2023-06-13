6efa9fe2d3289bc06c8f6c86f7fb78721837088a
nishita-esrat <nishitaesrat120@gmail.com>
2023-06-13 19:53:23 +0600

    linear-searching

---
+++ Searching/linear-searching.js
@@ -0,0 +1,15 @@
+let arr = [23, 45, 67, 45, 676, 34, 34, 56, 73, 78];
+
+let target = 56;
+
+function linearSearching(arr, target) {
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] === target) {
+      return `${target} founded index on ${index}`;
+    }
+  }
+
+  return "not founded";
+}
+
+console.log(linearSearching(arr, target));
