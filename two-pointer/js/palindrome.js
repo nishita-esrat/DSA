a75d3a68e57875da47df9978052f9a3a711371a8
nishita-esrat <nishitaesrat120@gmail.com>
2025-03-18 13:48:20 +0600

    palindrome using two pointer algo in js

--- two-pointer/js/palindrome.js
+++ two-pointer/js/palindrome.js
@@ -1,5 +1,19 @@
-const str = madam;
+const str = "madam";
 
-function palindromeTwoPointerAlgo(str) {}
+function palindromeTwoPointerAlgo(str) {
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start <= end) {
+    if (str[start] != str[end]) {
+      return false;
+    }
+
+    start++;
+    end--;
+  }
+
+  return true;
+}
 
 console.log(palindromeTwoPointerAlgo(str));
