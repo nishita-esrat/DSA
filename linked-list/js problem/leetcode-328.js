731bfeab89ff2b6b70e9d4598a8244dd35481c48
nishita-esrat <nishitaesrat120@gmail.com>
2025-03-28 13:50:52 +0600

    leet code 328 in js

---
+++ linked-list/js problem/leetcode-328.js
@@ -0,0 +1,19 @@
+//328. Odd Even Linked List
+
+function singleLinkedListLeetCode328(head) {
+  if (!head || !head.next) return head;
+
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+  return head;
+}
