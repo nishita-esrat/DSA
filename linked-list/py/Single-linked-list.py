cc9e7b5f9ff16ac5daa2443edc826c1fedbcbadf
nishita-esrat <nishitaesrat120@gmail.com>
2025-04-06 16:18:43 +0600

    append_at_position , update_value , print_list , find_node single linked list using py

--- linked-list/py/Single-linked-list.py
+++ linked-list/py/Single-linked-list.py
@@ -3,10 +3,8 @@ class Node:
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
+ 
 
-
-    def __repr__(self):  # Or  __str__
-        return f"Node(val={self.val})"
 
 
 class Single_linked_list:
@@ -14,7 +12,8 @@ class Single_linked_list:
         self.head = self.tail = Node(val) if val is not None else None
         self.len = 1 if val is not None else 0
 
-    def append(self, val):
+
+    def append_value(self, val):
         node = Node(val)
         if self.head is None:  
             self.head = self.tail = node
@@ -23,7 +22,8 @@ class Single_linked_list:
             self.tail = node
         self.len += 1 
 
-    def prepend(self,val):
+
+    def prepend_value(self,val):
         node = Node(val)
         if self.head is None:
             self.head = self.tail = node
@@ -34,14 +34,79 @@ class Single_linked_list:
         self.len += 1
 
 
+    def append_at_position(self,val,index):
+        if index < 1 or index > self.len + 1:
+          print("invalid index")
+          return
+        
+        if index == 1:
+          self.prepend_value(val)
+          return 
+        
+        if index == self.len + 1:
+          self.append_value(val)
+          return
+        
+        node = Node(val)
+        previous_node = self.find_node(index-1)
+        temp = previous_node.next
+        previous_node.next = node
+        node.next = temp
+        self.len += 1        
+
+
+    def find_node(self,index):
+
+        if index < 1 or index > self.len:
+          print("index is out of range")
+          return
+        
+        data = self.head
+        num = 1
+
+        while data:
+          if num == index:
+            break
+          
+          num +=1
+          data = data.next
+        
+        return data
+    
+
+    def update_value(self,val,index):
+     
+      if index < 1 or index > self.len:
+        print("index is out of range")
+        return 
+     
+      node = self.find_node(index)
+      node.val = val
+    
+
+    def print_list(self):
+       node = self.head
+       while node:
+        print(node.val, end=" -> ")
+        node = node.next
+       print("None")
 
 
 
-list = Single_linked_list()
-list.append(10)
-list.append(20)
-list.prepend(5)
-print("head",list.head)
-print("tail",list.tail)
-print("len",list.len)
+
+
+
+my_list = Single_linked_list()
+my_list.append_value(10)
+my_list.append_value(20)
+my_list.prepend_value(5)
+# print("find node",my_list.find_node(4))
+my_list.append_at_position(1,1)
+my_list.append_at_position(25,5)
+my_list.append_at_position(15,4)
+my_list.update_value(1555,4)
+my_list.print_list()
+print("head",my_list.head.val)
+print("tail",my_list.tail.val)
+print("len",my_list.len)
 
