146e7007559fa9832fb5d936698d7a97a203f68e
nishita-esrat <nishitaesrat120@gmail.com>
2025-05-26 19:38:24 +0600

    binary tree using js

---
+++ binary-tree/js/BST.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BTS {
+  constructor(value) {
+    this.root = new Node(value);
+  }
+
+  insert(value) {
+    let currentNode = this.root;
+
+    while (true) {
+      if (currentNode.value < value) {
+        if (!currentNode.right) {
+          let node = new Node(value);
+          currentNode.right = node;
+          break;
+        }
+
+        currentNode = currentNode.right;
+      } else {
+        if (!currentNode.left) {
+          let node = new Node(value);
+          currentNode.left = node;
+          break;
+        }
+
+        currentNode = currentNode.left;
+      }
+    }
+  }
+
+  search(target) {
+    let node = this.root;
+
+    while (node) {
+      if (node.value === target) {
+        console.log(`${target} is found`)
+        return `${target} is found`;
+      }
+
+      if (node.value < target) {
+        if (!node.right) {
+          return "Not Found";
+        }
+        node = node.right;
+      }
+
+      if (node.value > target) {
+        if (!node.left) {
+          return "Not Found";
+        }
+        node = node.left;
+      }
+    }
+  }
+
+}
+
+const bts = new BTS(10);
+bts.insert(9);
+bts.insert(11);
+bts.insert(8);
+bts.insert(12);
+bts.search(1)
+console.log(bts.search(1))
+
+console.log(bts);
