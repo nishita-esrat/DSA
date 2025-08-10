f2105729121c141f1f4e6e76e8eff99c194428c3
nishita-esrat <nishitaesrat120@gmail.com>
2025-08-10 22:18:23 +0600

    graph in py

--- graph/py/Graph.py
+++ graph/py/Graph.py
@@ -1,1 +1,40 @@
 
+class Graph:
+    def __init__(self):
+        self.graph = {}
+        self.visited = {}
+        self.parents = {}
+        self.stack = []
+
+    def add_gp(self, a, b, is_directed=False):
+        # Ensure both nodes exist in the graph dictionary
+        if a not in self.graph:
+            self.graph[a] = []
+        if b not in self.graph:
+            self.graph[b] = []
+
+       # Initialize visited and parents
+        if a not in self.visited:
+           self.visited[a] = False
+        if b not in self.visited:
+           self.visited[b] = False
+        if a not in self.parents:
+           self.parents[a] = None
+        if b not in self.parents:
+           self.parents[b] = None
+
+        # Add the edge
+        self.graph[a].append(b)
+        if not is_directed:
+           self.graph[b].append(a)
+
+gp = Graph()
+gp.add_gp(1,2)
+print(gp.graph)
+   
+
+    
+        
+        
+
+       
