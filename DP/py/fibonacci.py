b6713e4054b08239ffdbd2eec34b41bbd758fc5d
nishita-esrat <nishitaesrat120@gmail.com>
2025-08-30 19:38:25 +0600

    fibonacci with DP in py

---
+++ DP/py/fibonacci.py
@@ -0,0 +1,20 @@
+
+memo = {}
+
+def fibonacci(n):
+ 
+ if n == 0 or n == 1:
+   return n
+ 
+ if n in memo:
+   return memo[n]
+ 
+ memo[n] = fibonacci(n-1)+fibonacci(n-2)
+ return memo[n] 
+
+
+print(fibonacci(5))
+print(memo)
+
+
+
